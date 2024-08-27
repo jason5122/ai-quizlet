f2ab9822d062373460abaeb33ce3839251e6b341
Jason Han <jjhan@alum.vassar.edu>
2024-08-26 21:26:50 -0400

    Add Vite + React template

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { FlashcardArray } from 'react-quizlet-flashcard';
+import './App.css';
+
+function App() {
+  const cards = [
+    {
+      id: 1,
+      frontHTML: (
+        <div>
+          What is the capital of <u>Alaska</u>?
+        </div>
+      ),
+      backHTML: <>Juneau</>,
+    },
+    {
+      id: 2,
+      frontHTML: <>What is the capital of California?</>,
+      backHTML: <>Sacramento</>,
+    },
+    {
+      id: 3,
+      frontHTML: <>What is the capital of New York?</>,
+      backHTML: <>Albany</>,
+    },
+    {
+      id: 4,
+      frontHTML: <>What is the capital of Florida?</>,
+      backHTML: <>Tallahassee</>,
+    },
+    {
+      id: 5,
+      frontHTML: <>What is the capital of Texas?</>,
+      backHTML: <>Austin</>,
+    },
+    {
+      id: 6,
+      frontHTML: <>What is the capital of New Mexico?</>,
+      backHTML: <>Santa Fe</>,
+    },
+    {
+      id: 7,
+      frontHTML: <>What is the capital of Arizona?</>,
+      backHTML: <>Phoenix</>,
+    },
+  ];
+
+  return (
+    <div>
+      <FlashcardArray cards={cards} />
+    </div>
+  );
+}
+
+export default App;
